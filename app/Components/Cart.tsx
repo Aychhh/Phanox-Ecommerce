12d94c30e7455a6289c4ab533ba842cec325923e
Aychhh <ternsjoe@gmail.com>
2026-01-11 11:07:40 -0800

    update

--- app/Components/Cart.tsx
+++ app/Components/Cart.tsx
@@ -17,9 +17,8 @@ const Cart = () => {
     quantities,
     setShowCart,
     cartItems,
-    incQty,
-    decQty,
-    qty,
+    toogleCartItem,
+    onRemove
   } = useContext(StoreContext);
   return (
     <div className="cart-wrapper">
@@ -69,17 +68,17 @@ const Cart = () => {
 
                     <div >
                       <p className="quantity-desc">
-                        <span className="minus">
+                        <span className="minus" onClick={() => toogleCartItem(item._id, "dec")}>
                           <AiOutlineMinus  />
                         </span>
-                        <span className="num">0</span>
-                        <span className="plus">
+                        <span className="num">{item.quantity}</span>
+                        <span className="plus" onClick={() => toogleCartItem(item._id, "inc")}>
                           <AiOutlinePlus  />
                         </span>
                       </p>
                     </div>
 
-                    <button type="button" className="remove-item">
+                    <button type="button" className="remove-item" onClick={() => onRemove(item)}>
                       <TiDeleteOutline/>
                     </button>
                   </div>
--- app/Context/AuthContext.tsx
+++ app/Context/AuthContext.tsx
@@ -1,31 +1,69 @@
 "use client"
-import { createContext, ReactNode, useState } from "react";
+import React, { createContext, ReactNode, useContext, useState } from "react";
 import toast from "react-hot-toast";
 
 interface StoreContextType {
-    
+    showCart : boolean;
+    setShowCart : React.Dispatch<React.SetStateAction<boolean>>;
+    cartItems : product[];
+    setCartItems : React.Dispatch<React.SetStateAction<product[]>>
+    onAdd : (product : product, quantity : number) => void;
+    toogleCartItem : (id : string, action : 'inc' | 'dec') => void;
+    incQty : () => void;
+    decQty : () => void;
+    totalPrice : number;
+    quantities : number;
+    setQuantities : React.Dispatch<React.SetStateAction<number>>
+    setQty : React.Dispatch<React.SetStateAction<number>>
+    qty : number;
+    onRemove : (product : product) => void
 }
-interface AppProvider {
-    children : ReactNode
+
+// const defaultState : StoreContextType = {
+//   showCart : false,
+//   setShowCart : () => {},
+//   cartItems : [],
+//   onAdd : () => {},
+//   toogleCartItem : () => {},
+//   incQty : () => {},
+//   decQty : () => {},
+//   totalPrice : 0,
+//   quantities : 0,
+//   qty : 1,
+//   onRemove : () => {},
+//   setCartItems : () => [],
+//   setQuantities : () => {},
+//   setQty : () => {}
+// }
+
+interface product {
+  name: string;
+  details: string;
+  price: number;
+  _id: string;
+  image: any[];
+  quantity: number;
 }
 
 const StoreContext = createContext<StoreContextType | undefined>(undefined);
 
 
 
-export const AppProvider = ({children} : AppProvider) => {
+export const AppProvider = ({children} : {children : ReactNode}) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState<product[]>([]);
   const [quantities, setQuantities] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
   const [qty, setQty] = useState(1);
 
+  let foundItems : product ;
 
-  const onAdd = (product : any, quantity : any) => {
+
+  const onAdd = (product : product, quantity : number) => {
     const checkCartItem = cartItems.find((item : any) => item._id === product._id)
 
-    setTotalPrice((prevTotalPrice : any) => prevTotalPrice + product.price * quantity)
-    setQuantities((prevQuantity : any) => prevQuantity + quantity)
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
+    setQuantities((prevQuantity) => prevQuantity + quantity)
 
     if(checkCartItem){
       const updateCartItem = cartItems.map((cartProduct : any) => {
@@ -43,6 +81,43 @@ export const AppProvider = ({children} : AppProvider) => {
     toast.success(`${qty} ${product.name} added to the cart`)
   }
 
+  const toogleCartItem = (id : string, action : 'inc' | 'dec') => {
+
+   const foundItems = cartItems.find((item : product) => item._id === id)
+
+    if(!foundItems) return;
+
+    const updateCartItem = cartItems.map((item : product) => {
+      if(item._id !== foundItems._id) return item
+
+      if(action === 'inc'){
+        return{...item, quantity : foundItems.quantity + 1}
+      }
+      if(action === 'dec' && item.quantity > 1){
+        return {...item, quantity : foundItems.quantity - 1}
+      }
+      return item;
+    })
+    setCartItems(updateCartItem)
+    if(action === 'inc'){
+      setTotalPrice((prev) => prev + foundItems.price)
+      setQuantities((prev) => prev + 1)
+    }
+    if(action === 'dec'){
+      setTotalPrice((prev) => prev - foundItems.price)
+      setQuantities((prev) => prev - 1)
+    }
+  }
+
+  const onRemove = (product : product) => {
+   const foundItems = cartItems.find((item : product) => item._id === product._id)
+   if(!foundItems) return;
+    const newCartItems = cartItems.filter((item : product) => item._id !== product._id)
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice - foundItems.price * foundItems.quantity)
+    setQuantities((prevTotalQty) => prevTotalQty - foundItems.quantity)
+    setCartItems(newCartItems)
+  }
   const incQty = () => {
     setQty((prev) => prev + 1);
   }
@@ -66,11 +141,23 @@ export const AppProvider = ({children} : AppProvider) => {
         incQty,
         decQty,
         onAdd,
-        totalPrice
+        totalPrice,
+        toogleCartItem,
+        onRemove
       }}>
         {children}
       </StoreContext.Provider>
     )
-}
+} 
 
 export default StoreContext;
+
+export const useStateContext = () => {
+  const context = useContext(StoreContext)
+    if (!context) {
+    throw new Error(
+      "useStateContext must be used within a StateContextProvider"
+    );
+  }
+  return context;
+}
